fd90fc35f919632b85e486867ae2381ef38c9a67
Ryan Helsing <ryanhelsing@gmail.com>
2017-11-13 17:53:15 -0500

    note

--- v1.rb
+++ v1.rb
@@ -3,7 +3,7 @@ require './theory.rb'
 # create doc that can be processed by node to create midi
 key = "Am" # G for major, Gm for G minor, G#m, G#, etc
 progression1 = "i ii iii iv v vi vii" #TODO: case sensitive
-progression2 = "i-7 ii-7 iii-7 iv-7 v-7 vi-7 vii-7" #TODO: case sensitive
+progression2 = "i-7 ii-7 iii-7 iv-7 v-7 vi-7 vii-7" #TODO: case insensitive
 
 puts Theory.new.progression(progression1, Theory.new.scale(key))
 puts Theory.new.progression(progression2, Theory.new.scale(key))
@@ -14,3 +14,8 @@ puts Theory.new.progression(progression2, Theory.new.scale(key))
 #how much to normalize, 0-100 0 is regular, 100 is as much notes in common to root as possible on a chord by chord basis
 
 #generate in format transferable to midi node
+
+#(-5, -aug, -dim, -min, -maj, sus2, -sus4) (-7, -6, -9, -add9)  (Bass Note:) (-1-11)
+
+# https://www.justinguitar.com/en/IM-136-BuildingMelodicPatterns.php + my own patterns, and ones form fave songs
+
